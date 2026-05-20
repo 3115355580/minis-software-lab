c66c1253ee5319d63714ed7dacd272c8a60daad1
3115355580 <3115355580@qq.com>
2026-05-20 16:38:59 +0800

    feat: share persisted Image2Studio images directly

---
+++ apps/Image2Studio/app/src/main/java/cc/minis/image2studio/ImageShareProvider.java
@@ -0,0 +1,29 @@
+package cc.minis.image2studio;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ImageShareProvider extends ContentProvider {
+    public boolean onCreate() { return true; }
+    public String getType(Uri uri) { return "image/png"; }
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) { return null; }
+    public Uri insert(Uri uri, ContentValues values) { return null; }
+    public int delete(Uri uri, String selection, String[] selectionArgs) { return 0; }
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) { return 0; }
+
+    public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
+        String name = uri.getLastPathSegment();
+        if (name == null || name.contains("/") || name.contains("..")) throw new FileNotFoundException("bad name");
+        File f = new File(getContext().getFilesDir(), "images/" + name);
+        if (!f.exists()) throw new FileNotFoundException(name);
+        return ParcelFileDescriptor.open(f, ParcelFileDescriptor.MODE_READ_ONLY);
+    }
+}
